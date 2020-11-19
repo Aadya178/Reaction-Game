cca96dd72f7cdee7e486bec3a1fd394a50b3bcd6
Aadya178 <55178494+Aadya178@users.noreply.github.com>
2020-11-20 00:50:37 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 function welcome(){
 	alert("welcome! Press any key or click on the screen to make pikachu jump over the pokeball.")
 }
-
+var de,ds;
 var character=
 document.getElementById("character");
 var block=
@@ -13,7 +13,7 @@ function jump(){
 	setTimeout(function(){
 		character.classList.remove("animate");
 		},500);
-	
+	ds=new Date();
 }
 var checkDead= setInterval(function(){
 	var characterTop=parseInt(window.getComputedStyle(character).getPropertyValue("top"));
@@ -21,6 +21,9 @@ var checkDead= setInterval(function(){
 	if(blockLeft<20 && blockLeft>0 && characterTop>=130){
 		block.style.animation="none";
 		block.style.display="none";
+		de=new Date();
+		var score=ds-de;
+		document.write("You made a highscore! ",score);
 		alert("You lose!  Refresh the page to play again.");
 	}
 },10);
